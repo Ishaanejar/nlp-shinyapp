bc9ffc52e1e4e1f97258d1987cbf6d1a665dd840
ishaanejar <isha.anejar@gmail.com>
2018-12-22 19:47:58 +0530

    hindi support added and code fomatted

--- server.R
+++ server.R
@@ -1,9 +1,9 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 # 11810105_isha
 
@@ -14,202 +14,218 @@ library(shiny)
 #ud_model_hindi <- udpipe_download_model(language = "hindi")  # secs
 # Define server logic required to draw a histogram
 
-options(shiny.maxRequestSize=30*1024^2)
+options(shiny.maxRequestSize = 30 * 1024 ^ 2)
 
-shinyServer(function(input, output,session) {
-  
-  set.seed=2092014 
+shinyServer(function(input, output, session) {
+  set.seed = 2092014
   
   ##############Read Data#######################
   data <- reactive({
-    if (is.null(input$file)) {return(NULL)}
+    if (is.null(input$file)) {
+      return(NULL)
+    }
     else {
       filePath <- input$file$datapath
-      fileText <- paste(readLines(filePath), collapse = "\n")
-      fileText  =  str_replace_all(fileText, "<.*?>", "") 
+      fileText <-
+        paste(readLines(filePath, encoding = "UTF-8"), collapse = "\n")
+      #windowsFonts(devanew=windowsFont("Devanagari new normal"))
+      
+      fileText  =  str_replace_all(fileText, "<.*?>", "")
       return(fileText)
     }
   })
- #################### Pos tags and Language######################################
+  #################### Pos tags and Language######################################
   postags <- reactive({
-    l<- input$lang
-    if(l=="en"){
-      postaglist=input$xposchoice
+    l <- input$lang
+    if (l == "en") {
+      postaglist = input$xposchoice
       return(postaglist)
-      }
+    }
     else{
-      postaglist=input$xposchoice
-      postaglist<- gsub("JJ","ADJ",postaglist)
-      postaglist<-gsub("NN","NOUN",postaglist)
-      postaglist<-gsub("NNP","PROPN",postaglist)
-      postaglist<-gsub("RB","ADV",postaglist)
-      postaglist<-gsub("VB","VERB",postaglist)
-    return(postaglist)
-      }
+      postaglist = input$xposchoice
+      postaglist <- gsub("JJ", "ADJ", postaglist)
+      postaglist <- gsub("NN", "NOUN", postaglist)
+      postaglist <- gsub("NNP", "PROPN", postaglist)
+      postaglist <- gsub("RB", "ADV", postaglist)
+      postaglist <- gsub("VB", "VERB", postaglist)
+      return(postaglist)
+    }
     return(postaglist)
     
   })
   
   ############Display input text###################333
-  output$text  <- renderPrint({
-    validate(
-    need(input$file$datapath != "", "Please upload a text file"))
-    return(readLines(input$file$datapath))   
-    })
-  
-################Annotated Data Formation##########################
-    anndata  <- reactive({
-   
-      #english_model = udpipe_load_model("D:/Isha/taba/TABA Tutorial/english-ud-2.0-170801.udpipe")  # file_model only needed       
-      #english_model = udpipe_load_model("D:\\CBA\\T12\\TABA\\test shiny\\test1\\english-ud-2.0-170801.udpipe")  # file_model only needed
-      #x <- udpipe_annotate(english_model, x = data())
-      
-    if (input$lang=="en") 
-      {
-      model = udpipe_load_model(".\\models\\english-ud-2.0-170801.udpipe")
-      #model = udpipe_load_model("D:\\CBA\\T12\\TABA\\test shiny\\test1\\english-ud-2.0-170801.udpipe")
-        }
-      else {
-        validate(
-          need(input$modelfile$datapath != "", "Please upload a model file")
-                  )  
-      model=udpipe_load_model(input$modelfile$datapath)
-      }
-      
-      validate(need(input$file$datapath != "", "Please upload a text file"))
-      x <- udpipe_annotate(model, x = data())
-      x <- as.data.frame(x)
-      return(x)
-  
+  output$text  <- renderUI({
+    validate(need(input$file$datapath != "", "Please upload a text file"))
+    return(data())
   })
- 
+  
+  ################Annotated Data Formation##########################
+  anndata  <- reactive({
+    if (input$lang == "en")
+    {
+      model = udpipe_load_model(".\\models\\english-ud-2.0-170801.udpipe")
+      
+    }
+    else {
+      validate(need(
+        input$modelfile$datapath != "",
+        "Please upload a model file"
+      ))
+      model = udpipe_load_model(input$modelfile$datapath)
+    }
+    
+    validate(need(input$file$datapath != "", "Please upload a text file"))
+    x <- udpipe_annotate(model, x = data())
+    x <- as.data.frame(x)
+    return(x)
+    
+  })
+  
   
   ################Annotated Text Rendering ##########
-     output$anntext  <- renderDataTable({ 
-       x<- anndata()
-       if(input$lang=="en"){
-       x1 = subset(x, xpos %in% postags())
-       }else{
-       x1 = subset(x, upos %in% postags())
-       }
-       return(x1)
-       })
-    
-###############NounExtraction######################
-  
-  output$phrasextractnoun<-renderTable({
-    all_nouns = anndata() %>% subset(., upos %in% "NOUN") 
-    top_nouns = txt_freq(all_nouns$lemma)  
-    head(top_nouns, 50)	
+  output$anntext  <- renderDataTable({
+    x <- anndata()
+    if (input$lang == "en") {
+      x1 = subset(x, xpos %in% postags())
+    } else{
+      x1 = subset(x, upos %in% postags())
+    }
+    return(x1)
   })
   
-######################### Verb Extraction###########################
-  output$phrasextractverb<-renderTable({
-    
-    all_verbs = anndata() %>% subset(., upos %in% "VERB") 
+  ###############NounExtraction######################
+  
+  output$phrasextractnoun <- renderDataTable({
+    all_nouns = anndata() %>% subset(., upos %in% "NOUN")
+    top_nouns = txt_freq(all_nouns$lemma)
+    head(top_nouns, 50)
+  })
+  
+  ######################### Verb Extraction###########################
+  output$phrasextractverb <- renderDataTable({
+    all_verbs = anndata() %>% subset(., upos %in% "VERB")
     top_verbs = txt_freq(all_verbs$lemma)
     head(top_verbs, 50)
- 
-  })
- ########################Collocations################################   
-    output$collocations<-renderTable({
-      
-      collocateddata <- keywords_collocation(x = anndata(),  
-                                             term = "token", 
-                                             group = c("doc_id", 
-                                                       "paragraph_id", "sentence_id"),
-                                             ngram_max = 4) 
-      collocateddata
-    }) 
     
-#########Looking at which words are followed by another word##############
+  })
+  ########################Collocations################################
+  output$collocations <- renderDataTable({
+    collocateddata <- keywords_collocation(
+      x = anndata(),
+      term = "token",
+      group = c("doc_id",
+                "paragraph_id", "sentence_id"),
+      ngram_max = 4
+    )
+    collocateddata
+  })
   
-  output$cooccurrences_noun_adj<-renderTable({
-    validate(need(length(input$xposchoice)>2 , "Please select atleast 2 xpos tags"))  
+  #########Looking at which words are followed by another word##############
   
-        cooccurrences_noun_adj <- cooccurrence(
-          if(input$lang=="en"){
-      x = subset(anndata(), xpos %in% postags())
-                                        }
-          else{ x = subset(anndata(), upos %in% postags())}, 
-      term = "lemma", 
-      group = c("doc_id", "paragraph_id", "sentence_id"))
+  output$cooccurrences_noun_adj <- renderDataTable({
+    validate(need(
+      length(input$xposchoice) > 2 ,
+      "Please select atleast 2 xpos tags"
+    ))
+    
+    cooccurrences_noun_adj <- cooccurrence(
+      if (input$lang == "en") {
+        x = subset(anndata(), xpos %in% postags())
+      }
+      else{
+        x = subset(anndata(), upos %in% postags())
+      },
+      term = "lemma",
+      group = c("doc_id", "paragraph_id", "sentence_id")
+    )
     return(cooccurrences_noun_adj)
     
-                                              })    
+  })
   
- #####Looking at which words are located in the same document/sentence/paragraph.
+  #####Looking at which words are located in the same document/sentence/paragraph.
   
-  output$gen_cooccur<-renderTable({
+  output$gen_cooccur <- renderDataTable({
+    adata = anndata()
+    validate(need(
+      length(input$xposchoice) > 2 ,
+      "Please select atleast 2 xpos tags"
+    ))
     
-      adata=anndata()
-    validate(need(length(input$xposchoice)>2 , "Please select atleast 2 xpos tags"))  
-   
-    if(input$lang=="en"){
-      gen_cooccur <- cooccurrence(x = adata$lemma, 
+    if (input$lang == "en") {
+      gen_cooccur <- cooccurrence(x = adata$lemma,
                                   relevant = adata$xpos %in% postags())
     }
     else{
-      gen_cooccur <- cooccurrence(x = adata$lemma, 
-                                  relevant = adata$upos %in% 
-                                    postags())}
+      gen_cooccur <- cooccurrence(x = adata$lemma,
+                                  relevant = adata$upos %in%
+                                    postags())
+    }
     return(gen_cooccur)
-                                })
+  })
   
   
-#####Looking at which words are in the neighbourhood of the word as in follows the word within skipgram number of words
+  #####Looking at which words are in the neighbourhood of the word as in follows the word within skipgram number of words
   
-  output$cooccur_skips_ngrams<-renderTable({
-    validate(need(length(input$xposchoice)>2 , "Please select atleast 2 xpos tags")) 
-    adata=anndata() 
-    if(input$lang=="en"){
-     cooccur_skips_ngrams <- cooccurrence(x = adata$lemma, 
-                                         relevant = adata$xpos %in% 
-                                           postags() , 
-                                         skipgram = 4) }
+  output$cooccur_skips_ngrams <- renderDataTable({
+    validate(need(
+      length(input$xposchoice) > 2 ,
+      "Please select atleast 2 xpos tags"
+    ))
+    adata = anndata()
+    if (input$lang == "en") {
+      cooccur_skips_ngrams <- cooccurrence(
+        x = adata$lemma,
+        relevant = adata$xpos %in%
+          postags() ,
+        skipgram = 4
+      )
+    }
     
-    else{   
-      cooccur_skips_ngrams <- cooccurrence(x = adata$lemma, 
-                                           relevant = adata$upos %in% 
-                                             postags() , 
-                                           skipgram = 4) 
+    else{
+      cooccur_skips_ngrams <- cooccurrence(
+        x = adata$lemma,
+        relevant = adata$upos %in%
+          postags() ,
+        skipgram = 4
+      )
     }
   })
+  
+  #############Co-occurence Graph#####################
+  
+  output$plot1 = renderPlot({
+    validate(need(
+      length(input$xposchoice) > 2 ,
+      "Please select atleast 3 xpos tags"
+    ))
+    cooccurrences_noun_adj <- cooccurrence(
+      if (input$lang == "en") {
+        x = subset(anndata(), xpos %in% postags())
+      } else{
+        x = subset(anndata(), upos %in% postags())
+      }
+      ,
+      term = "lemma",
+      group = c("doc_id", "paragraph_id", "sentence_id")
+    )
     
-#############Co-occurence Graph#####################
+    wordnetwork <- head(cooccurrences_noun_adj, 50)
+    wordnetwork <- igraph::graph_from_data_frame(wordnetwork)
     
-    output$plot1 = renderPlot({
-      validate(need(length(input$xposchoice)>2 , "Please select atleast 3 xpos tags")) 
-        cooccurrences_noun_adj <- cooccurrence(  
-           if(input$lang=="en"){
-        x = subset(anndata(), xpos %in% postags())}else{x = subset(anndata(), upos %in% postags())}
-                , 
-        term = "lemma", 
-        group = c("doc_id", "paragraph_id", "sentence_id"))
-      
-      wordnetwork <- head(cooccurrences_noun_adj, 50)
-      wordnetwork <- igraph::graph_from_data_frame(wordnetwork)
-      
-      ggraph(wordnetwork, layout = "auto") +  
-        geom_edge_link(aes(width = cooc, edge_alpha = cooc), 
-                       edge_colour = "orange") +  
-        geom_node_text(aes(label = name), col = "black", size = 5) +
-        theme_graph(base_family = "Arial") +  
-        theme(legend.position = "none") +
-        labs(title = "Graph upto with 3 levels", 
-             subtitle = "Nouns & Adjective")->g
-      
-      return(g)    
-    })
+    ggraph(wordnetwork, layout = "auto") +
+      geom_edge_link(aes(width = cooc, edge_alpha = cooc),
+                     edge_colour = "orange") +
+      geom_node_text(aes(label = name), col = "black", size = 5) +
+      theme_graph(base_family = "Arial") +
+      theme(legend.position = "none") +
+      labs(title = "Graph upto with 3 levels",
+           subtitle = "Nouns & Adjective") -> g
     
-    
-    
-    
-  }) 
+    return(g)
+  })
   
   
   
   
-      
-
-
+})
--- ui.R
+++ ui.R
@@ -1,101 +1,132 @@
-#11810105_isha_
+#11810105_Isha Rukhaya
+#11810058_Nitin Agarwal
+#11810013_Ajay Chopra
 # This is the user-interface definition of a Shiny web application. You can
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
+
 library(shinyWidgets)
 library(shiny)
 library(udpipe)
-#library(textrank)
-#library(lattice)
 library(igraph)
 library(ggraph)
 library(ggplot2)
-#library(wordcloud)
 library(stringr)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  tags$style(HTML("
-        .tabs-above > .nav > li[class=active] > a {
-                  background-color: #000;
-                  color: #FFF;
-                  }")),
+  tags$style(
+    HTML(
+      "
+      .tabs-above > .nav > li[class=active] > a {
+      background-color: #000;
+      color: #FFF;
+      }"
+)
+    ),
 
 # Application title
-  titlePanel("NLP using UDPipe"),
+titlePanel("NLP using UDPipe"),
 
 #Sidebar with 3 input options : Upload file and model file and checkbox choice for xpos tagging
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Lets start text analysis by uploading a text file and specify Language"),
-
-
-      ## Language selection radio button
-      awesomeRadio(
-        inputId = "lang", label = "Please select Language of Text ",
-        choices = c("en", "other"),selected = ("en"),
-        inline = TRUE, status = "warning"
-      ),
-      
-      
-      ### Upload utilities
-      
-      fileInput("file", "Upload text file"),
-      fileInput("modelfile", "Upload model file for other than English language"),
-      
-      
-
-
-## Checkbbox input for pos tag filtering
-
-       awesomeCheckboxGroup(
-         inputId = "xposchoice", label= "Choose Xpos tags: It will filter over Annotation and Cooccurence graph only",choices = c("JJ", "NN", "NNP", "RB","VB"),selected = list("JJ","NN","NNP")
-       
-        )
-      
-     
-      # checkboxGroupInput(label= "Choose Xpos tags",inputId = "xposchoice",
-      #                    choiceNames =
-      #                      list("adjective (JJ)","noun(NN)","proper noun (NNP)",
-      #                           "adverb (RB)","verb (VB)"),selected = list("JJ","NN","NNP"),
-      #                    choiceValues =
-      #                      list("JJ", "NN", "NNP", "RB","VB")
-      # )
-      ),
+sidebarLayout(
+  sidebarPanel(
+    helpText(
+      "Lets start text analysis by uploading a text file and specify Language"
+    ),
     
     
-# Main Panel
-    mainPanel(
-      setBackgroundImage(src = "D://Isha//taba//TABA Tutorial//asgmt//retinapixel.jpg"),
-      tabsetPanel(type = "tabs",
-                  tabPanel(title= "Raw Text", h4('This App displays step by step process of doing NLP using udpipe model.UDPipe provides the standard NLP functionalities of tagging, parsing and dependency evaluations - all within R'),
-                           br(),
-                           h4(p("Follow tabs sequence from left to Right")),
-                           h4(p("Pl. upload Text file and Model File from the left panel and see the file content in this tab  
-                                and then click the next tab. 
-                                By default this app takes English model as input and will process the text with the model.")),
-                           br(), textOutput("text"))
-
-                  ,tabPanel("Annotated text", dataTableOutput("anntext"))
-                  ,tabPanel("Nouns extraction", uiOutput("phrasextractnoun")),
-                  tabPanel("Verbs extraction", uiOutput("phrasextractverb"))
-                  ,tabPanel(title = "Collocations",h4("Collocations are Sequence of words or terms that co-occur more often than would be expected by chance."), uiOutput("collocations"))
-                  ,tabPanel("Cooccurrences of Noun and Adjectives", uiOutput("cooccurrences_noun_adj"))
-                  , tabPanel("Cooccurrences at the file level", uiOutput("gen_cooccur"))
-                  , tabPanel("Skipgram based Co-occurrences", uiOutput("cooccur_skips_ngrams"))
-                  , tabPanel("Cooccurence graph", plotOutput("plot1"))
-                  # tabPanel(title="Chosen XpoS Tags","XPoS description: adjective (JJ)
-                  #          noun(NN)
-                  #          proper noun (NNP)
-                  #          adverb (RB)
-                  #          verb (VB)" ,br(), uiOutput("txt"))
-                  #tabPanel("Preformatted",pre(includeText("include.txt")))
+    ## Language selection radio button
+    awesomeRadio(
+      inputId = "lang",
+      label = "Please select Language of Text ",
+      choices = c("en", "other"),
+      selected = ("en"),
+      inline = TRUE,
+      status = "warning"
+    ),
+    
+    
+    ### Upload utilities
+    
+    fileInput("file", "Upload text file(only .txt file)"),
+    fileInput("modelfile", "Upload model file for other than English language"),
+    
+    
+    
+    
+    ## Checkbbox input for pos tag filtering
+    
+    awesomeCheckboxGroup(
+      inputId = "xposchoice",
+      label = "Choose Xpos tags: It will filter over Annotation and Cooccurence graph only",
+      choices = c("JJ", "NN", "NNP", "RB", "VB"),
+      selected = list("JJ", "NN", "NNP")
+      
+    )
+    
+  ),
+  
+  
+  # Main Panel
+  mainPanel(
+    setBackgroundImage(src = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/59/27/6957a41f6e761b3.jpg"),
+    tabsetPanel(
+      type = "tabs",
+      tabPanel(
+        title = "Introduction",
+        h4(
+          'This App displays step by step process of doing NLP using udpipe model.UDPipe provides the standard NLP functionalities of tagging, parsing and dependency evaluations - all within R'
+        ),
+        br(),
+        h4(p("Follow tabs sequence from left to Right")),
+        h4(
+          p(
+            "Please upload Text file and Model File from the left panel and see the file content in this tab
+            and then click the next tab."
+          ),
+          div("This app doesn't require model to be uploaded for English .", style =
+                "color:blue")
+          ),
+        br(),
+        uiOutput("text")
+    )
+    
+    ,
+    tabPanel("Annotated text", dataTableOutput("anntext"))
+    ,
+    tabPanel("Nouns extraction", dataTableOutput("phrasextractnoun")),
+    tabPanel("Verbs extraction", dataTableOutput("phrasextractverb"))
+    ,
+    tabPanel(
+      title = "Collocations",
+      h4(
+        "Collocations are Sequence of words or terms that co-occur more often than would be expected by chance."
+      ),
+      dataTableOutput("collocations")
+    )
+    ,
+    tabPanel(
+      "Cooccurrences of Noun and Adjectives",
+      dataTableOutput("cooccurrences_noun_adj")
+    )
+    ,
+    tabPanel(
+      "Cooccurrences at the file level",
+      dataTableOutput("gen_cooccur")
+    )
+    ,
+    tabPanel(
+      "Skipgram based Co-occurrences",
+      dataTableOutput("cooccur_skips_ngrams")
+    )
+    ,
+    tabPanel("Cooccurence graph", plotOutput("plot1"))
       )
-      )
-  )
+    )
 )
-)
+    ))
